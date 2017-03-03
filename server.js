53ea1db3bc2cb345fce35c577caef1e2d8459c43
Gav <gavin@parity.io>
2017-03-03 18:55:40 +0100

    Fix everything.

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const Parity = require('@parity/parity.js');
 const oo7_parity = require('oo7-parity');
 function setupAbi(url) {
 	const transport = new Parity.Api.Transport.Http(url);
-	const api = new Parity.Api(transport);
-	api.abi = oo7_parity.abiPolyfill();
+	var api = new Parity.Api(transport);
+	api.abi = oo7_parity.abiPolyfill(api);
 	let bonds = oo7_parity.setupBonds(api);
 	return [api, bonds];
 }
 let [apiF, bondsF] = setupAbi('http://localhost:8545');
 let [apiK, bondsK] = setupAbi('http://localhost:8546');
+bondsF.netChain.then(c => console.log(`On network chain ${c}`));
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -23,27 +24,57 @@ const address = '0x4d6Bb4ed029B33cF25D0810b029bd8B1A6bcAb7B';
 
 // TODO!!! UPDATE registry ABI in oo7-parity.js
 
-let sms = bondsF.makeContract(bondsF.registry.lookupAddress('smsverification', 'A'), BadgeABI);
+const BadgeABI = [{"constant":true,"inputs":[{"name":"_who","type":"address"},{"name":"_field","type":"string"}],"name":"getData","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"},{"name":"_field","type":"string"}],"name":"getAddress","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"},{"name":"_field","type":"string"}],"name":"getUint","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"}],"name":"certified","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"}],"name":"Confirmed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"}],"name":"Revoked","type":"event"}];
+let sms = bondsF.makeContract(bondsF.registry.lookupAddress('emailverification', 'A'), BadgeABI);
 
-app.post('/:address', function (req, res) {
-	let who = req.params.address;
-	if (who.match(/^0x[a-fA-F0-9]{40}$/)) {
-		sms.certified(who).then(certified => {
-			if (certified) {
-				res.end('Kovan Ether on its way');
+let banned = {
+};
+
+let past = {
+};
+
+function rain(who, to, res) {
+	if (who.match(/^0x[a-f0-9]{40}$/) && to.match(/^0x[a-f0-9]{40}$/)) {
+		if (!banned[who]) {
+			if (!past[who] || Date.now() - past[who] > 24 * 3600 * 1000) {
+				sms.certified(who).then(certified => {
+					if (certified) {
+						past[who] = Date.now();
+						apiK.eth.sendTransaction({ from: address, to: to, value: 50000000000000000000 })
+							.then(tx => res.end(`Kovan Ether on its way in transaction ${tx}`))
+							.catch(e => res.end(`Internal error: ${JSON.stringify(e)}`));
+					} else {
+						res.end('Account not certified');
+					}
+				});
 			} else {
-				res.end('Account not certified');
+				res.end('Faucet draw rate limited. Call back in 24 hours.');
 			}
-		});
+
+		} else {
+			res.end('Account banned.');
+		}
 	} else {
 		res.end('Invalid address.');
 	}
+}
+
+app.get('/:address', function (req, res) {
+	let who = req.params.address.toLowerCase();
+	rain(who, who, res);
 });
 
-var server = app.listen(80, function () {
+app.get('/:address/:to', function (req, res) {
+	let who = req.params.address.toLowerCase();
+	let to = req.params.to.toLowerCase();
+	rain(who, to, res);
+});
+
+
+
+console.log("Start server...");
+var server = app.listen(4080, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("sms-faucet service listening at http://%s:%s", host, port);
 });
-
-const BadgeABI = [{"constant":true,"inputs":[{"name":"_who","type":"address"},{"name":"_field","type":"string"}],"name":"getData","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"},{"name":"_field","type":"string"}],"name":"getAddress","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"},{"name":"_field","type":"string"}],"name":"getUint","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_who","type":"address"}],"name":"certified","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"}],"name":"Confirmed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"}],"name":"Revoked","type":"event"}];
